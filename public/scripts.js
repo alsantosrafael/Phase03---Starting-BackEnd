83a374e20b45ccb7486ffb3e6150b28a0e82260d
Rafael Almeida <rafael.profeng@gmail.com>
2020-06-30 11:05:24 -0300

    First Commit. Starting Backend, nodemon, template engines, server.

---
+++ public/scripts.js
@@ -0,0 +1,22 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+
+const cards = document.querySelectorAll('.card')
+
+
+
+for(let card of cards) {
+
+    card.addEventListener("click", function() {
+        const videoId = card.getAttribute("id")//pegando o atributo id do card para usar como endereco do video
+        modalOverlay.classList.add('active')//adicionando classe active para o modal poder ser aberto
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${videoId}`//iframe//to adicionando o endereco do video de forma dinamica
+
+    })
+
+}
+
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove('active')//Removendo a classe active para sumir com o modal quando eu fechar
+    modalOverlay.querySelector("iframe").src = ""//Aqui eu estou fazendo com q o video pare na hora que eu fechar
+})
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express') // Criamos variável para chamar express
+const nunjucks = require('nunjucks')
+
+const server = express()// criamos uma variavel para executar o express
+
+server.use(express.static('public')) 
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res){
+    return res.render("index")
+
+})
+
+server.get("/about", function(req, res){ //Criando rotas para a pagina about
+    return res.render("about")
+})
+
+
+server.get("/content", function(req, res){ //Criando rotas para a pagina content
+    return res.render("content")
+})
+
+server.use(function(req, res) { //Chamando nossa página de erro
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function (){
+    console.log("Server is running")
+
+})
